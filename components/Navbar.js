b10b2ffdf89aa5d635d5d6d6a6e08b8aa1a32db6
Pritam Singh Negi <preetamnegi7@gmail.com>
2023-05-05 20:43:44 +0530

    Added Responsive Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,8 +7,15 @@ import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import { useRouter } from 'next/router';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import { Box } from '@mui/material';
+import { Box, useMediaQuery, useTheme } from '@mui/material';
 import { styled } from '@mui/system';
+import MenuIcon from '@mui/icons-material/Menu';
+import IconButton from '@mui/material/IconButton';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
 
 const NavBarContainer = styled('div')(({ theme }) => ({
   flexGrow: 1,
@@ -34,28 +41,74 @@ const StyledButton = styled(Button)(({ theme }) => ({
 
 const Navbar = () => {
   const router = useRouter();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
 
   const handleButtonClick = (route) => {
     router.push(route);
+    if (isMobile) {
+      setDrawerOpen(false);
+    }
   };
+  const listItems = [
+    {text: 'Home', route: '/'},
+    {text: 'Multi-Dimensional Stack-up', route: '/multidimensionalstackup'},
+    {text: 'About', route: '/about'},
+  ];
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={() => setDrawerOpen(false)}
+      onKeyDown={() => setDrawerOpen(false)}
+    >
+
+      {listItems.map((item, index) => (
+  <ListItem button key={item.text} onClick={() => handleButtonClick(item.route)}>
+    <ListItemText primary={item.text} />
+  </ListItem>
+))}
+    </Box>
+  );
 
   return (
     <NavBarContainer>
       <AppBar position="static">
         <Toolbar>
           <NavBarTitle variant="h6">Tolerance Stack-Up Calculator</NavBarTitle>
-          <Box sx={{ flexGrow: 1 }} />
-          <CenteredBox>
-            <StyledButton onClick={() => handleButtonClick('/')}>Home</StyledButton>
-            <StyledButton onClick={() => handleButtonClick('/multidimensionalstackup')}>
-              Multi-Dimensional Stack-up
-            </StyledButton>
-            <StyledButton onClick={() => handleButtonClick('/about')}>About</StyledButton>
-          </CenteredBox>
-          <Box sx={{ flexGrow: 1 }} />
+          {isMobile ? (
+            <>
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ ml: 2 }}
+                onClick={() => setDrawerOpen(true)}
+              >
+                <MenuIcon />
+              </IconButton>
+              <SwipeableDrawer
+                anchor={'right'}
+                open={drawerOpen}
+                onClose={() => setDrawerOpen(false)}
+                onOpen={() => setDrawerOpen(true)}
+              >
+                {list()}
+              </SwipeableDrawer>
+            </>
+          ) : (
+            <CenteredBox>
+              <StyledButton onClick={() => handleButtonClick('/')}>Home</StyledButton>
+              <StyledButton onClick={() => handleButtonClick('/multidimensionalstackup')}>
+                Multi-Dimensional Stack-up
+              </StyledButton>
+              <StyledButton onClick={() => handleButtonClick('/about')}>About</StyledButton>
+            </CenteredBox>
+          )}
         </Toolbar>
       </AppBar>
-      <Box sx={{ flexGrow: 1 }} />
       <BottomNavigation showLabels sx={{ width: '100%', position: 'fixed', bottom: 0 }}>
         <BottomNavigationAction
           sx={{ marginLeft: 'auto' }}
